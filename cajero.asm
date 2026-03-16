5d69c657bfc93edc2d83ad16b056e6584ea0b321
sharafayyoub <ahmadsharaf249@gmail.com>
2026-03-16 15:42:55 +0100

    cambios sharaf

--- cajero.asm
+++ cajero.asm
@@ -2,7 +2,7 @@ sys_exit  equ 1
 sys_read  equ 3
 sys_write equ 4
 
-segment .data
+section .data
 saldo db 50
 menu_principal db '1) Inicio', 10
                db '2) Consutar saldo', 10
@@ -16,8 +16,8 @@ opcion db 'Introduce una opcion: '
 opcion_len equ $ - opcion
 
 
-segment .bss
-num1 resb 2 
+section .bss
+num1 resb 3
 num_ingreso resb 3
 num_retiro resb 2
 
@@ -33,13 +33,22 @@ mov ecx,menu_principal
 mov edx,menu_len
 int 0x80                ;para imprimir el menu 
 
+mov eax,sys_write
+mov ebx,1
+mov ecx,opcion
+mov edx,opcion_len      ;para imprimir selecciona opcion
+
 mov eax,sys_read
 mov ebx,0
 mov ecx,num1
-mov edx,2
+mov edx,3
+int 0x80            ; para que pueda introducir numero
+
 
 mov eax, sys_exit
+mov ebx, 0
 int 0x80                ;el exit 
+
 
 
 
