03ffead092812600cbf88b1aa36a575725a047f0
sharafayyoub <ahmadsharaf249@gmail.com>
2026-04-19 12:20:42 +0200

    issue menus

--- cajero.asm
+++ cajero.asm
@@ -4,8 +4,9 @@ sys_write equ 4
 
 section .data
 saldo db 50
+
 menu_principal db '1) Inicio', 10
-               db '2) Consutar saldo', 10
+               db '2) Consultar saldo', 10
                db '3) Ingresar dinero', 10
                db '4) Retirar dinero', 10
                db '5) Configurar PIN', 10
@@ -15,86 +16,128 @@ menu_len equ $ - menu_principal
 opcion db 'Introduce una opcion: '
 opcion_len equ $ - opcion
 
-msg_inicio db 'Has elegido Inicio', 10
+msg_inicio   db 'Has elegido Inicio', 10
 msg_inicio_len equ $ - msg_inicio
 
-msg_saldo db 'Has elegido Consultar saldo', 10
+msg_saldo    db 'Has elegido Consultar saldo', 10
 msg_saldo_len equ $ - msg_saldo
 
-msg_ingreso db 'Has elegido Ingresar dinero', 10
+msg_ingreso  db 'Has elegido Ingresar dinero', 10
 msg_ingreso_len equ $ - msg_ingreso
 
-section .bss
-num1 resb 3
-num_ingreso resb 3
-num_retiro resb 2
+msg_retiro   db 'Has elegido Retirar dinero', 10
+msg_retiro_len equ $ - msg_retiro
 
+msg_pin      db 'Has elegido Configurar PIN', 10
+msg_pin_len equ $ - msg_pin
+
+msg_invalida db 'Opcion no valida', 10
+msg_invalida_len equ $ - msg_invalida
+
+section .bss
+num1       resb 3
+num_ingreso resb 3
+num_retiro  resb 2
 
 section .text
 global _start
+
 _start:
 
+menu_loop:
+    ; Imprimir menú
+    mov eax, sys_write
+    mov ebx, 1
+    mov ecx, menu_principal
+    mov edx, menu_len
+    int 0x80
 
-mov eax,sys_write
-mov ebx,1
-mov ecx,menu_principal
-mov edx,menu_len
-int 0x80                ;para imprimir el menu 
+    ; Imprimir "Introduce una opcion"
+    mov eax, sys_write
+    mov ebx, 1
+    mov ecx, opcion
+    mov edx, opcion_len
+    int 0x80
 
-mov eax,sys_write
-mov ebx,1
-mov ecx,opcion
-mov edx,opcion_len      ;para imprimir selecciona opcion
-int 0x80 
+    ; Leer opción
+    mov eax, sys_read
+    mov ebx, 0
+    mov ecx, num1
+    mov edx, 3
+    int 0x80
 
-mov eax,sys_read
-mov ebx,0
-mov ecx,num1
-mov edx,3
-int 0x80            ; para que pueda introducir numero
+    cmp byte [num1], '1'
+    je opcion_inicio
 
-cmp byte [num1], '1'
-je opcion_inicio        ; comparar primer caracter introducido con '1'
+    cmp byte [num1], '2'
+    je opcion_saldo
 
-cmp byte [num1], '2'
-je opcion_saldo         ; comparar opcion 2
+    cmp byte [num1], '3'
+    je opcion_ingreso
 
-cmp byte [num1], '3'
-je opcion_ingreso       ; comparar opcion 3
+    cmp byte [num1], '4'
+    je opcion_retiro
 
-; si no es 1, salir directamente
-jmp salir
+    cmp byte [num1], '5'
+    je opcion_pin
 
+    cmp byte [num1], '6'
+    je salir
+
+    jmp opcion_invalida     ; si no coincide con nada
 
 opcion_inicio:
-mov eax, sys_write
-mov ebx, 1
-mov ecx, msg_inicio
-mov edx, msg_inicio_len
-int 0x80
-jmp salir
+    mov eax, sys_write
+    mov ebx, 1
+    mov ecx, msg_inicio
+    mov edx, msg_inicio_len
+    int 0x80
+    jmp menu_loop           ; volver al menú
 
 opcion_saldo:
-mov eax, sys_write
-mov ebx, 1
-mov ecx, msg_saldo
-mov edx, msg_saldo_len
-int 0x80
-jmp salir
+    mov eax, sys_write
+    mov ebx, 1
+    mov ecx, msg_saldo
+    mov edx, msg_saldo_len
+    int 0x80
+    jmp menu_loop
 
 opcion_ingreso:
-mov eax, sys_write
-mov ebx, 1
-mov ecx, msg_ingreso
-mov edx, msg_ingreso_len
-int 0x80
-jmp salir
+    mov eax, sys_write
+    mov ebx, 1
+    mov ecx, msg_ingreso
+    mov edx, msg_ingreso_len
+    int 0x80
+    jmp menu_loop
 
+opcion_retiro:
+    mov eax, sys_write
+    mov ebx, 1
+    mov ecx, msg_retiro
+    mov edx, msg_retiro_len
+    int 0x80
+    jmp menu_loop
+
+opcion_pin:
+    mov eax, sys_write
+    mov ebx, 1
+    mov ecx, msg_pin
+    mov edx, msg_pin_len
+    int 0x80
+    jmp menu_loop
+
+opcion_invalida:
+    mov eax, sys_write
+    mov ebx, 1
+    mov ecx, msg_invalida
+    mov edx, msg_invalida_len
+    int 0x80
+    jmp menu_loop           ; volver al menú también
 
 salir:
-mov eax, sys_exit
-mov ebx, 0
-int 0x80
+    mov eax, sys_exit
+    mov ebx, 0
+    int 0x80
 
 
 
